5b05b6420de1b0d7b0ea878e22af3c1b5e4d0b3d
FrancoLab <francolabuschagne90@gmail.com>
2024-02-27 08:04:17 +0200

    Add initial project setup for Vite, TypeScript, React, and TailwindCSS with ESLint configuration and logo assets

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+	return (
+		<div className="flex flex-col p-6 max-w-7xl mx-auto">
+			<div className="flex w-full flex-col items-center space-y-4">
+				<div>
+					<img src="/defijn_logo.svg" alt="Defijn Logo" />
+				</div>
+				<h3 className="text-xl tracking-wider">
+					sandbox and playground
+				</h3>
+			</div>
+			<div>{/* Components testing here */}</div>
+		</div>
+	);
+}
+
+export default App;
